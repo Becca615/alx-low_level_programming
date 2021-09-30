aca725124f6ca31cfc15ed1cddbdd2971d15164f
Becca615 <damilolarebecca8@gmail.com>
2021-09-30 03:38:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+
 /**
-* main - entry for  c coding challenge
-* in low level coding section.
-* Return: 0 returns zero
-*/
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/**
-	 * This code puts the below line.
-	*/
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
