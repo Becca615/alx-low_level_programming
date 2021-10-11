6493fbdd9cefb27db1bb4f8a4ef05875144f6433
Becca615 <damilolarebecca8@gmail.com>
2021-10-11 08:28:40 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar as a message
+ * main - Prints Holberton as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
 	int count, sz;
 
 	sz = sizeof(str) / sizeof(int);
