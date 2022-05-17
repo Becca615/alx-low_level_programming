b0fc25377b078c04018041e169ff46cc754c63d1
Becca615 <damilolarebecca8@gmail.com>
2022-05-17 14:35:38 -0700

    Create static library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -c *.c
-<<<<<<< HEAD
-ar -rc liball.a *.o
-=======
 ar -rc liball.a *.o
 ranlib liball.a
->>>>>>> 3b117ff5b83097fb0b4148efddb8fe552c939230
